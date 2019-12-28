ed06e739e9532d285af59c5b16d097e72fb04053
kundan kumar <kundanbhum@gmail.com>
2019-12-29 02:38:18 +0530

    database added

---
+++ app/config/database.js
@@ -0,0 +1,7 @@
+const { Client } = require('pg'); 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+client.connect();
+module.exports = client;
--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const db = require('./app/config/database');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var PORT = process.env.PORT;
 
 app.get("/", (req,res) => {
+    let loginTable = "CREATE  TABLE USER  ( " +
+        "`firstName` INT  AUTOINCREMENT " +
+        "`lastName`  VARCHAR(150) NOT NULL " +
+        "`mobileNumber` VARCHAR(10)" +
+        "`email` VARCHAR(50)" +
+        "`password` VARCHAR(255" + 
+        "PRIMARY KEY ('mobileNumber') )"
+    let res = await db.query(loginTable, )
    res.json({
        status: 200,
        message: "Welcome to Costly Server !",
